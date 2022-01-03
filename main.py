e66fd4d9de9746ff9728303ca6b5bf4b25a8efbc
shijimimegane <54845164+shijimimegane@users.noreply.github.com>
2022-01-04 01:49:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ class Stream_Listener_V2(object):
                 mark = text.find(':参戦ID')
                 raid_id = text[mark - 9:mark - 1]
                 st.write(raid_id)
-                subprocess.call('xsel -bi < raid_id')
+                subprocess.call('raid_id')
                 #pyperclip.copy(raid_id)
 
 listener = Stream_Listener_V2()
