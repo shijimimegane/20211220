d7962bb9c32c78121ab26524260f33c8a0572d23
shijimimegane <54845164+shijimimegane@users.noreply.github.com>
2022-01-06 13:23:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import requests
 import json
 import streamlit as st
-import clipboard
+import streamlit.components.v1 as components
+
+html_string = '''
+<h1>HTML string in RED</h1>
+
+<script language="javascript">
+  document.querySelector("h1").style.color = "red";
+  console.log("Streamlit runs JavaScript");
+  alert("Streamlit runs JavaScript");
+</script>
+'''
+
+components.html(html_string)
 
 dict_values = {'マグナN': ['Lv60 ティアマト・マグナ',
   'Lv80 コロッサス・マグナ',
