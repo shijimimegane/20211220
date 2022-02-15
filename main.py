51df74ca87d33845858fc12c9353ae6bbc2f68d0
shijimimegane <54845164+shijimimegane@users.noreply.github.com>
2022-02-15 18:39:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ slot = st.empty()
 subprocess.call(["cat", "/etc/os-release"])
 
 # subprocess.call(["Xvfb", ":1", "-screen", "0", "1024x768x24"])
-subprocess.call("export DISPLAY=127.0.0.1:0", shell=True)
+subprocess.call("export DISPLAY=118.83.215.89:0.0", shell=True)
 subprocess.call("echo -e 'hello\nworld\n' | xclip -selection clipboard", shell=True)
 
 
