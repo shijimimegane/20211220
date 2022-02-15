0ee679246b0639464bf287b6dacbecc54ae5985f
shijimimegane <54845164+shijimimegane@users.noreply.github.com>
2022-02-16 08:32:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import streamlit.components.v1 as components
 
 t = mycomponent(my_input_value="a")
 st.write(t)
-if t.value == "Clicked":
+if len(t) != 0:
   st.write(t * 2)
 
 dict_values = {'マグナN': ['Lv60 ティアマト・マグナ',
