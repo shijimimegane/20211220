043b60d7c19322c11fcd6609009e416637132d64
shijimimegane <54845164+shijimimegane@users.noreply.github.com>
2022-02-15 19:00:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ slot = st.empty()
 
 # subprocess.call(["cat", "/etc/os-release"])
 
+st.write(os.environ.get('DISPLAY'))
 
-subprocess.call("echo DISPLAY", shell=True)
+subprocess.call("echo $DISPLAY", shell=True)
 # subprocess.call(["Xvfb", ":0", "-screen", "0", "1024x768x24"])
 # subprocess.call("export $DISPLAY=118.83.215.89:0.0", shell=True)
 # subprocess.call("xhost +118.83.215.89:0.0", shell=True)
