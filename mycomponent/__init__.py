27bfd3ba62998cd0674edcf2120b8333a97df2d9
shijimimegane <54845164+shijimimegane@users.noreply.github.com>
2022-02-16 10:50:41 +0900

    Update __init__.py

--- mycomponent/__init__.py
+++ mycomponent/__init__.py
@@ -1,9 +1,5 @@
 import streamlit.components.v1 as components
-_component_func = components.declare_component(
+mycomponent = components.declare_component(
     "mycomponent",
     path="./mycomponent",
 )
-
-def my_component(name, key=None):
-    component_value = _component_func(name=name, key=key, default=0)
-    return component_value
